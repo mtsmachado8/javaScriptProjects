227761eb8136bc1ae170b20f3271543d27995394
Mateus <mtsmachado8@gmail.com>
2018-11-01 07:37:15 -0300

    Functions constructors, Obj.create, typeof and instanceOf

---
+++ jsWeirdParts/Initial Class/app.js
@@ -0,0 +1,12 @@
+function waitThreeSeconds(){
+	let ms = 3000 + new Date().getTime();
+	while(new Date() < ms){}
+		console.log('finished function waitThreeSeconds');
+}
+
+document.addEventListener('click', () => {
+	console.log('click event');
+});
+
+waitThreeSeconds();
+console.log('Finished');
